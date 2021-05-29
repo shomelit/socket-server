1f40b74051f79695b6ca162ac3bc42e50830258d
Anais Bello <shomelit@gmail.com>
2021-05-29 00:13:17 -0500

    Primer Commit Rest Server

---
+++ README.md
@@ -0,0 +1,14 @@
+# Socket-Server
+
+Reconstruir módulos de Node
+...
+npm install
+
+General el DIST
+...
+tsc -w
+
+Levantar servidor (cualquiera de los dos comandos)
+...
+nodemon dist/
+node dist/
---
+++ index.ts
@@ -0,0 +1,20 @@
+
+import Server from './class/server';
+import router from './routes/routes';
+import express from 'express';
+import cors from 'cors';
+
+const server = new Server();
+
+//BodyParser
+server.app.use(express.urlencoded({extended:true}));
+server.app.use(express.json());
+
+//CORS
+server.app.use(cors({origin:true, credentials:true}));
+
+// Rutas de servicios
+server.app.use('/', router);
+server.start(()=>{
+    console.log(`Servidor corriendo en el puerto ${server.port}`);
+});
